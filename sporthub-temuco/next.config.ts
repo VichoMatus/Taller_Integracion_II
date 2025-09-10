c30c5e3efd44afda1258aed0f160b48eaf28c769
Diego24Vas <dvasquez2024@alu.uct.cl>
2025-09-09 23:16:00 -0300

    Se agrega en docker pgadmin, y se arreglan credenciales para docker en .env

--- sporthub-temuco/next.config.ts
+++ sporthub-temuco/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    "http://localhost:19006",
+    "http://localhost:8081",
+    "http://localhost:3000"
+  ]
 };
 
 export default nextConfig;
