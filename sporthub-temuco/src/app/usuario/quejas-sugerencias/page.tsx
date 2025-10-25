7a145ba224d6ef27fdd2e016ad1ead2e9d581b4d
SPereda2005 <spereda2024@alu.uct.cl>
2025-10-25 11:35:32 -0300

    SideBar casi completo
    Se le implementa useAuthStatus al sidebar para que dependiendo del estado de sesion del usuario este contenga menos informacion relacionada al usuario, ademas se quita el boton de cerrar sesion en caso de que no haya una sesion activa, ademas las redirecciones quedan hechas.

---
+++ sporthub-temuco/src/app/usuario/quejas-sugerencias/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+
+export default function QuejasSugerenciasPage() {
+  return (
+    <div>
+      <h1>Quejas y Sugerencias</h1>
+      <p>🚧 Página en construcción</p>
+      {/* Implementar después */}
+    </div>
+  );
+}
